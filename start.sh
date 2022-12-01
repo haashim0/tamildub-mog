e7256b3f1d02ee9354467d026c6f9e55d0daab9a
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-01 06:38:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/haashim007/EvaMaria.git /EvaMaria
+  git clone https://github.com/haashim0/tamildub-mog.git /tamildub-mog
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /tamildub-mog
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
